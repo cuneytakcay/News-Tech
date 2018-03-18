627b3eea8c8c6a26f71660fc3ccbd4629bec1d42
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-18 16:09:12 -0700

    Modified index.js for ES6

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'News-Tech Article Scraper' });
-});
+router.get('/', (req, res, next) => {
+  res.render('home', { title: 'News-Tech Article Scraper' })
+})
 
-module.exports = router;
+module.exports = router
