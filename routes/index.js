f71a7e3c6e768b861a88a49f1d954e352ef8e1f0
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-19 15:47:17 -0700

    Modified index.js to grab data from the database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express')
-var router = express.Router()
+// Require dependencies 
+const express = require( 'express' )
+const router = express.Router()
 
-/* GET home page. */
+// Require models
+const db = require('../models')
+
+// GET route to bring articles from database to the homepage
 router.get('/', (req, res, next) => {
-  res.render('home', { title: 'Read the latest tech articles' })
+	// Grab all articles from database
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If successfully find Articles, send them to the homepage
+      //res.json(dbArticle);
+      res.render('home', { 
+      	articles: dbArticle
+      })
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 })
 
 module.exports = router
