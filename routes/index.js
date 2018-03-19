ed5203285853305d1b607a13caa20f944ef75f27
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-19 13:13:53 -0700

    Changed the title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('home', { title: 'News-Tech Article Scraper' })
+  res.render('home', { title: 'Read the latest tech articles' })
 })
 
 module.exports = router
