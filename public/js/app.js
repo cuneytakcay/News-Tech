3c8ad95334261a4d761b4da353dd992948b10d2e
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-19 16:33:39 -0700

    Remodified the ajax call

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,13 @@
 $('#scraper').on('click', () => {
-	// Ajax call when scraper button is hit to start scraping
-	// and grabing the json results from /scrape api to display
-	$.getJSON('/scrape', (data) => {
-		for (var i = 0; i < data.length; i++) {
-			$('#article-container')
-				.append(`
-					<div>
-						<a href='${data[i].link}'>
-							${data[i].title}
-						</a>
-						<button id="save-btn" class="btn btn-sm btn-primary float-right">Save article</button>
-					</div>
-				`)	
-		}
-	})
+  $.ajax({
+    method: 'GET',
+    url: '/scrape'
+  }).then(() => {
+      console.log('DONE!');
+      // Reload the page to get the updated list
+      location.reload()
+    }
+  )
 })
 
 
