9c9e96d831b0328c3c61dc841ca175ba21532b30
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-19 00:59:27 -0700

    Modified app.js to list articles on the homepage

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-console.log('testing')
+$('#scraper').on('click', () => {
+	// Ajax call when scraper button is hit to start scraping
+	// and grabing the json results from /scrape api to display
+	$.getJSON('/scrape', (data) => {
+		for (var i = 0; i < data.length; i++) {
+			$('#list')
+				.append(`
+					<li>
+						<a href='${data[i].link}'>
+							${data[i].title}
+						</a>
+					</li>
+				`)	
+		}
+	})
+})
+
