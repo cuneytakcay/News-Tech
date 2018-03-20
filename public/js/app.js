7649413fbb6b7837ecd32ae7f8659d8f91eccf11
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-19 18:44:17 -0700

    Added ajax call to save articles

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,26 @@ $('#scraper').on('click', () => {
   $.ajax({
     method: 'GET',
     url: '/scrape'
-  }).then(() => {
+  }).then((data) => {
       console.log('DONE!');
       // Reload the page to get the updated list
       location.reload()
     }
   )
+})
+
+// When save article button is clicked, that article is saved in database
+$('.save-btn').on('click', 'button', () => {
+	// Save the id of the button
+	const id = $(this).attr('data-id')
+
+	// Ajax call for the article
+	$.ajax({
+		method: 'GET',
+		url: '/articles/' + id
+	}).then((data) => {
+		console.log('Article was saved!')
+	}) 
 })
 
 
