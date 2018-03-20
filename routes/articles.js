905071ce323519601b3505addd539e24953ceee4
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-20 09:13:54 -0700

    modified articles route and home.hbs

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+// Require dependencies 
+const express = require( 'express' )
+const router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Require models
+const db = require('../models')
 
-module.exports = router;
+// POST route
+router.post('/', (req, res, next) => {
+  const article = {
+    title: req.body.title,
+    link: req.body.link
+  }
+	// Save article into mongoDB collection
+  db.Collection.create(article)
+    .then(dbCollection => {
+      console.log(dbCollection)
+    })
+    .catch(err => {
+      // If an error occurred, send it to the client
+      res.json(err)
+    })
+    res.send('Collection created')
+})
+
+// Export router
+module.exports = router
